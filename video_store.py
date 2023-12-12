715290cd183f7a76198131636e950e31fa02e1c2
yellzombie <87000920+yellzombie@users.noreply.github.com>
2023-12-12 22:07:43 +0545

    Can click video
    Aba video bata banaune ho kta ho

---
+++ video_store.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+cam = cv.VideoCapture(0)
+cc = cv.VideoWriter_fourcc(*'mp4v')
+file = cv.VideoWriter('output.mp4', cc, 15.0, (640, 480))
+if not cam.isOpened():
+   print("error opening camera")
+   exit()
+while True:
+   # Capture frame-by-frame
+   ret, frame = cam.read()
+   frame = cv.flip(frame, 1)
+   # if frame is read correctly ret is True
+   if not ret:
+      print("error in retrieving frame")
+      break
+   img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+   cv.imshow('frame', img)
+   file.write(img)
+
+   
+   if cv.waitKey(1) == ord('q'):
+      break
+
+cam.release()
+file.release()
+cv.destroyAllWindows()
