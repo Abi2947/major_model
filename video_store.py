a3bc7bcd78450df0197e46e8500ec765883fac5e
yellzombie <87000920+yellzombie@users.noreply.github.com>
2023-12-25 14:06:29 +0545

    Deleted BGR2RGB

--- video_store.py
+++ video_store.py
@@ -1,27 +1,15 @@
-#pip install opencv-contrib-python
-import cv2 as cv
-cam = cv.VideoCapture(0)#for linux user put the value to 1 instead of 0
-cc = cv.VideoWriter_fourcc(*'mp4v')
-file = cv.VideoWriter('dataset/Training/output.mp4', cc, 15.0, (640, 480))
-if not cam.isOpened():
-   print("error opening camera")
-   exit()
-while True:
-   # Capture frame-by-frame
-   ret, frame = cam.read()
-   frame = cv.flip(frame, 1)
-   # if frame is read correctly ret is True
-   if not ret:
-      print("error in retrieving frame")
-      break
-   img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-   cv.imshow('frame', img)
-   file.write(img)
-
-   
-   if cv.waitKey(1) == ord('q'):
-      break
-
-cam.release()
-file.release()
-cv.destroyAllWindows()
+import cv2
+cap = cv2.VideoCapture(0) 
+cap.set(3,640)
+cap.set(4,480)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 15.0, (640,480))
+while(True):
+     ret, frame = cap.read()
+     out.write(frame)
+     cv2.imshow('frame', frame)
+     if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
